7835e69136645714ee45a69af7dbc8284084c770
Danny-Vu <vudinhbao.nz@gmail.com>
2020-07-07 12:43:48 +0700

    Initial Commit

---
+++ GameStore/GameStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStoreApp.swift
+//  GameStore
+//
+//  Created by Bao Vu on 7/7/20.
+//
+
+import SwiftUI
+
+@main
+struct GameStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
